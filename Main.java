a84317c5a328cd7718c6742ecd570c037cf8f3f8
Ayoub Manai <92479177+AyoubManai01@users.noreply.github.com>
2024-09-20 15:03:12 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        Produit produit0 = new Produit();
+        Produit produitA = new Produit(1021, "Lait", "Delice");
+        Produit produitB = new Produit(2510, "Yaourt", "Vitalait");
+        Produit produitC = new Produit(3250, "Tomate", "Sicam", 1.200);
+
+        produit0.afficher();
+        produitA.afficher();
+        produitB.afficher();
+        produitC.afficher();
+
+        produitA.prix = 0.700;
+        produitA.afficher();
+
+        produitB.prix = 0.600;
+        produitB.afficher();
+
+        System.out.println(produitA.toString());
+        System.out.println(produitB.toString());
+        // toString doit etre appelé comme parametre du fonction println, au contraire du
+        // methode afficher() qui ne necessite pas l'appel du println
+
+        Date expirationDate = new Date(2025, 11, 31);
+        produitA.dateexp = expirationDate;
+        produitB.dateexp = expirationDate;
+        produitC.dateexp = expirationDate;
+        System.out.println(produitA.dateexp);
+    }
+
+}
---
+++ Produit.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Produit {
+    int identifiant;
+    String marque;
+    String libelle;
+    double prix;
+    Date dateexp;
+    Produit(){}
+    Produit(int identifiant, String marque, String libelle, double prix){
+            this.identifiant = identifiant;
+            this.marque = marque;
+            this.libelle = libelle;
+            this.prix = prix;
+    }
+    Produit(int identifiant, String marque, String libelle){
+        this.identifiant = identifiant;
+        this.marque = marque;
+        this.libelle = libelle;
+    }
+    Produit(int identifiant, String marque, String libelle, double prix, Date dateexp){
+        this.identifiant = identifiant;
+        this.marque = marque;
+        this.libelle = libelle;
+        this.prix = prix;
+        this.dateexp = dateexp;
+    }
+    void afficher(){
+        System.out.println("Identifiant: " + this.identifiant);
+        System.out.println("Marque: " + this.marque);
+        System.out.println("Libelle: " + this.libelle);
+        System.out.println("Prix: " + this.prix);
+    }
+
+    @Override
+    public String toString(){
+        return this.identifiant + " " + this.marque + " " + this.libelle + " " + this.prix;
+    }
+}
+
+
